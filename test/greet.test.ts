b1fed7ea1e157b947e7ab10c5ce6bbb61ae0c2ef
chrismguevara <chrismguevara@gmail.com>
2025-07-21 17:42:39 -0700

    testing commit and push from stackblitz

--- test/greet.test.ts
+++ test/greet.test.ts
@@ -4,6 +4,6 @@ import { greet } from '../src';
 describe('greet', () => {
   test('should say hello world when given world', () => {
     const result = greet('world');
-    expect(result).toBe('Hello world');
+    expect(result).toBe('Hello world2');
   });
 });
